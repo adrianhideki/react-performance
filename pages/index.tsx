94edeb0794e738859f89d742308c25caeedbb7eb
Adrian Hideki <adrian.hideki.br@gmail.com>
2021-05-31 20:52:44 -0300

    Criando componentes base

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,35 @@
+import { FormEvent, useState } from "react";
+import { SearchResults } from "../Components/SearchResults";
+
 export default function Home() {
-  return <h1>Teste</h1>;
+  const [search, setSearch] = useState("");
+  const [results, setResults] = useState([]);
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+
+    if (!search.trim()) {
+      return;
+    }
+
+    const response = await fetch(`http://localhost:3333/products?q=${search}`);
+    const data = await response.json();
+
+    setResults(data);
+  };
+
+  return (
+    <div>
+      <h1>Search</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button type="submit">Pesquisar</button>
+      </form>
+      <SearchResults results={results}/>
+    </div>
+  );
 }
